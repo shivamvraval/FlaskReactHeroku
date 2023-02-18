fb193130880c842b9e3ee9755c442bbdcae19bb0
Shivam-raval96 <sraval@fas.harvard.edu>
2023-02-17 22:40:21 -0500

    adding backend2

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask.helpers import send_from_directory
 from multiprocessing import reduction
 from flask import Flask, url_for, json
 from flask_cors import CORS,cross_origin #comment this on deployment
-#from flask_restful import reqparse
 import pandas as pd
 import io
 from sklearn.manifold import TSNE
