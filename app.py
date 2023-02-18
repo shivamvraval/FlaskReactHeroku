7c610b07902e02560191c3e73917124baaee14cd
Shivam-raval96 <sraval@fas.harvard.edu>
2023-02-17 22:50:10 -0500

    adding backend2

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
+from flask import Flask
 from flask.helpers import send_from_directory
-#from multiprocessing import reduction
-from flask import Flask, url_for, json
-from flask_cors import CORS,cross_origin #comment this on deployment
-'''import pandas as pd
-import io
-from sklearn.manifold import TSNE
-import umap
-from ast import literal_eval
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import SVC
-import numpy as np
-import heapq
-
-import openai'''
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder='my-app/build', static_url_path='')
 CORS(app)
@@ -31,8 +18,3 @@ def serve():
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
-    
